fdb0370ef78bf01a47adf690432b6c9b9f371ff9
anna-zau <annazauholnikova555@gmail.com>
2022-12-22 17:03:14 +0200

    commir

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { About } from '../pages/About';
+import { Products } from '../pages/Products';
+import { NotFound } from '../pages/NotFound';
+
+import { StyledLink, Logo, Header, Container } from './App/App.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{' '}
+          GoMerch Store
+        </Logo>
+        <nav>
+          <StyledLink to="/" end>
+            Home
+          </StyledLink>
+          <StyledLink to="/about">About</StyledLink>
+          <StyledLink to="/products">Products</StyledLink>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Container>
   );
 };
